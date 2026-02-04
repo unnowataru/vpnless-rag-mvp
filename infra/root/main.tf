8c862e2cdd46fbea84d169e61fe171ae7ddfc47b
Unno Wataru <unnow@networld.co.jp>
2026-02-04 17:05:53 +0900

    security: restrict bedrock invoker to gemma model arn

--- infra/root/main.tf
+++ infra/root/main.tf
@@ -29,3 +29,38 @@ resource "aws_budgets_budget" "monthly" {
     subscriber_email_addresses = ["unnow@networld.co.jp"]
   }
 }
+
+# --- Bedrock invoker (app/on-prem) ---
+resource "aws_iam_user" "bedrock_invoker" {
+  name = "rag-bedrock-invoker"
+}
+
+data "aws_iam_policy_document" "bedrock_invoker" {
+  statement {
+    sid    = "InvokeBedrockModels"
+    effect = "Allow"
+    actions = [
+      "bedrock:InvokeModel",
+      "bedrock:InvokeModelWithResponseStream"
+    ]
+    resources = ["arn:aws:bedrock:ap-northeast-1::foundation-model/google.gemma-3-4b-it"]
+  }
+
+  # Optional: allow listing models for troubleshooting
+  statement {
+    sid       = "ListModels"
+    effect    = "Allow"
+    actions   = ["bedrock:ListFoundationModels"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "bedrock_invoker" {
+  name   = "rag-bedrock-invoker-policy"
+  policy = data.aws_iam_policy_document.bedrock_invoker.json
+}
+
+resource "aws_iam_user_policy_attachment" "bedrock_invoker" {
+  user       = aws_iam_user.bedrock_invoker.name
+  policy_arn = aws_iam_policy.bedrock_invoker.arn
+}
